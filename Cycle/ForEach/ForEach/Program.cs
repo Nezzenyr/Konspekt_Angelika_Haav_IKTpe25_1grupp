4ebe10c26a10047dad0fe764e441e84c419aec7b
Nezzenyr <nezzenyr@gmail.com>
2025-11-10 10:33:33 +0200

    foreach

--- Cycle/ForEach/ForEach/Program.cs
+++ Cycle/ForEach/ForEach/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace ForEach
 {
     internal class Program
-    {
-        static void Main(string[] args)
+    {string[] konsoolid = { "ps1", "nes", "n64" };
+
+        List<string> mängud = new List<string>();
+        foreach(string konsool in konsoolid)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(konsool);
         }
-    }
+
+        string currentEntry = ""; 
+
+        while (currentEntry == "")
+            {
+            Console.WriteLine("Sisesta mäng, või kirjuta" \"Ei taha\" kui soovid esitust lõpetada")
+            currentEntry = Console.ReadLine();
+            
+            if (currentEntry == "ei taha")
+            {
+            break;
+            }
+}
+        mängud.Add(currentEntry);
+currentEntry = "";
+
+         Console.WriteLine("Mis on sinu lemikvärvid, sisesta palun ükshaaval \nKui rohkem värve ei ole, kirjuta \"rohkem pole\"");
+    }    
+
 }
