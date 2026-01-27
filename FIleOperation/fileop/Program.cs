8218c81202ffcfbcc2efcd985e332d2ca4e9f31c
Opilane TTHK <opilane@tthk.ee>
2026-01-27 09:24:13 +0200

    ye

---
+++ FIleOperation/fileop/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace fileop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta siia oma postkasti aadress");
+
+            string filePath = "C:\\Users\\opilane\\source\\repos\\Conspect\\Konspekt_Angelika_Haav_IKTpe25_1grupp\\FIleOperation\\postkastiaadress.txt";
+            string aadress = Console.ReadLine();
+
+            File.WriteAllText(filePath, aadress);
+
+            Console.WriteLine("Kõik laulusõnad");
+
+            DisplayThisFile();
+
+            private static void DisplayThisFile()
+            {
+            string line = "";
+            try
+            {
+                using (StreamReader readThisThing = new StreamReader
+             C:\\Users\\opilane\\source\\repos\\Conspect\\Konspekt_Angelika_Haav_IKTpe25_1grupp\\FIleOperation\\MJBJ.txt)
+            }
+             catch (Exception e)
+            {
+                Console.WriteLine("Ei saa lugeda sest:" + e.Message);
+                throw;
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             }
+       }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Kalulaator 
+
+int arv1 = 0;
+int arv2 = 0;
+Console.WriteLine("sisesta esimene arv");
+arv1 = int.Parse(Console.ReadLine());
+Console.WriteLine("sisesta teine arv");
+arv2 = int.Parse(Console.ReadLine());
+int liitmine = arv1 + arv2;
+Console.WriteLine("missugust tehet soovid teha? kirjuta kas: + - * / ^");
+string tehtetüüp = Console.ReadLine();
+int tehe = 0;
+if (tehtetüüp == "+")
+{
+    tehe = arv1 + arv2;
+}
+if (tehtetüüp == "-")
+{
+    tehe = arv1 - arv2;
+}
+if (tehtetüüp == "*")
+{
+    tehe = arv1 * arv2;
+}
+if (tehtetüüp == "/")
+{
+    tehe = arv1 / arv2;
+}
+if (tehtetüüp == "^")
+{
+    tehe = (int)Math.Pow(arv1, arv2);
+}
+Console.WriteLine($" {tehtetüüp} tehte tulemus: {tehe}");
+
